7338c0d2bc73a7fba413db1651820e6e7c5d3398
13069807232 <2581775404@qq.com>
2025-08-30 15:02:03 +0800

    新增功能：运行项目后自动打开网页

--- 运行项目.bat
+++ 运行项目.bat
@@ -1,2 +1,4 @@
 cd backend
+start "" powershell -WindowStyle Hidden -NoProfile -Command "Start-Sleep -Seconds 5; Start-Process 'http://localhost:8080/'"
+start "" powershell -WindowStyle Hidden -NoProfile -Command "Start-Sleep -Seconds 5; Start-Process 'http://localhost:8080/h2-console'"
 "D:\apache-maven-3.9.9\bin\mvn.cmd" spring-boot:run -DskipTests
